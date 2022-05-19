ba5ea214c818dfdf9cd629283bd3dcbd5e1c5826
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-05-19 13:44:36 +0200

    init modal

--- src/Cards.js
+++ src/Cards.js
@@ -7,14 +7,16 @@ function Card(props) {
   const name = props.character.name;
   const image = props.character.image;
   return (
-    <div className="flip-card">
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <img src={image} alt="Avatar" />
-        </div>
-        <div className="flip-card-back" key={props.character.id}>
-          <h1>{name}</h1>
-          <Button variant="outline-info">Info</Button>{" "}
+    <div className="container">
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src={image} alt="Avatar" />
+          </div>
+          <div className="flip-card-back" key={props.character.id}>
+            <h1>{name}</h1>
+            <Button variant="outline-info">More Info</Button>{" "}
+          </div>
         </div>
       </div>
     </div>
